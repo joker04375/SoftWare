e49aca111b69fc514c4943a05faedbdc874b1716
wo <1354933501@qq.com>
2022-11-01 21:36:42 +0800

    model

--- src/main/java/com/example/ruangong/dao/CartDao.java
+++ src/main/java/com/example/ruangong/dao/CartDao.java
@@ -2,11 +2,13 @@ package com.example.ruangong.dao;
 
 import com.example.ruangong.pojo.Cart;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
 @Mapper
+@Repository
 public interface CartDao {
     int addCart(Cart cart);
 
--- src/main/java/com/example/ruangong/service/impl/CartServiceImpl.java
+++ src/main/java/com/example/ruangong/service/impl/CartServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.ruangong.service.impl;
 import com.example.ruangong.dao.CartDao;
 import com.example.ruangong.pojo.Cart;
 import com.example.ruangong.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CartServiceImpl implements CartService {
+    @Autowired
     private CartDao cartDao;
 
     @Override
