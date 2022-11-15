105951c9086fa7e9c9a2392ed86594973a224b5b
jocker04375 <985319769@qq.com>
2022-11-15 21:13:30 +0800

    interceptor

--- src/main/java/com/example/ruangong/configure/WebConfig.java
+++ src/main/java/com/example/ruangong/configure/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/register");
     }
 }
---
+++ src/main/java/com/example/ruangong/interceptor/LoginInterceptor.java
@@ -0,0 +1,41 @@
+package com.example.ruangong.interceptor;
+
+import com.example.ruangong.Utils.UserHolder;
+import com.example.ruangong.pojo.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在
+        if(user==null){
+            //不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+        //存在，保存用户到ThreadLocal
+        UserHolder.saveUser((User) user);
+
+        if(UserHolder.getUser()==null){
+            response.setStatus(401);
+            return false;
+        }
+        //放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
